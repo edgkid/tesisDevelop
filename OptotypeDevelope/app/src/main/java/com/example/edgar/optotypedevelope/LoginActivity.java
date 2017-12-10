e3c82a311504ce6db37a8ce584b4bd52e30f56f3
Edgar-PC\Edgar <edglandaeta.15@gmail.com>
2017-12-10 18:42:30 -0430

    se crea interfaz de login y se inicializa su clase asociada

--- OptotypeDevelope/app/src/main/java/com/example/edgar/optotypedevelope/LoginActivity.java
+++ OptotypeDevelope/app/src/main/java/com/example/edgar/optotypedevelope/LoginActivity.java
@@ -1,13 +1,38 @@
 package com.example.edgar.optotypedevelope;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText    editTextUserName;
+    EditText    editTextPaswword;
+    Button      buttonLogin;
+    ImageView   imageViewIcon;
+    Context     contextActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        contextActivity = this;
+
+        editTextUserName = (EditText) findViewById(R.id.editTextUserNameLogin);
+        editTextPaswword = (EditText) findViewById(R.id.editTextViewPassWord);
+        imageViewIcon   = (ImageView) findViewById(R.id.imageViewIconEye);
+
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener((View.OnClickListener) contextActivity);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
