4afa3569442179cb10e03e26f395d92b635581b7
Edgar-PC\Edgar <edglandaeta.15@gmail.com>
2017-12-10 19:54:36 -0430

    se asocian elementos del dashBoard

--- OptotypeDevelope/app/src/main/java/com/example/edgar/optotypedevelope/DashBoardActivity.java
+++ OptotypeDevelope/app/src/main/java/com/example/edgar/optotypedevelope/DashBoardActivity.java
@@ -1,13 +1,39 @@
 package com.example.edgar.optotypedevelope;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
 
-public class DashBoardActivity extends AppCompatActivity {
+public class DashBoardActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button logOut;
+    Button update;
+    ImageView imageUser;
+
+    ListView listViewMenu;
+    Context contextActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
+
+        contextActivity = this;
+        logOut = (Button) findViewById(R.id.buttonLogout);
+        update = (Button) findViewById(R.id.buttonUpdate);
+        imageUser = (ImageView) findViewById(R.id.imageViewLoginUser);
+        logOut.setOnClickListener((View.OnClickListener) contextActivity);
+        update.setOnClickListener((View.OnClickListener) contextActivity);
+        listViewMenu = (ListView) findViewById(R.id.listViewDashBoardMenu);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
