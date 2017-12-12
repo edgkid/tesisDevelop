f108e3b01248e39f238e30a67d259e807630c6d2
Edgar-PC\Edgar <edglandaeta.15@gmail.com>
2017-12-11 21:58:50 -0430

    se crea helper para tabla de optotipos

---
+++ OptotypeDevelope/app/src/main/java/com/example/edgar/optotypedevelope/OptotypeDbHelper.java
@@ -0,0 +1,57 @@
+package com.example.edgar.optotypedevelope;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by Edgar on 11/12/2017.
+ */
+
+public class OptotypeDbHelper extends  DbApp {
+
+    public OptotypeDbHelper(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String sql ="";
+        ContentValues values = new ContentValues();
+
+        // crear Tablas de base de datos
+        sql = sql + "CREATE TABLE " + OptotypeDbContract.OptotypeEntry.TABLE_NAME + " ( ";
+        sql = sql + OptotypeDbContract.OptotypeEntry._ID + " INTEGER PRIMARY KEY, ";
+        sql = sql + OptotypeDbContract.OptotypeEntry.ID + " TEXT, ";
+        sql = sql + OptotypeDbContract.OptotypeEntry.OPTOTYPECODE + " TEXT, ";
+        sql = sql + OptotypeDbContract.OptotypeEntry.OPTOTYPENAME + " TEXT, ";
+        sql = sql + OptotypeDbContract.OptotypeEntry.IMAGE + " TEXT, ";
+        sql = sql + " UNIQUE ( " + OptotypeDbContract.OptotypeEntry.ID + " ) ";
+        sql = sql +" ) ";
+
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        String sql ="";
+        ContentValues values = new ContentValues();
+
+        // crear Tablas de base de datos
+        sql = sql + "CREATE TABLE " + OptotypeDbContract.OptotypeEntry.TABLE_NAME + " ( ";
+        sql = sql + OptotypeDbContract.OptotypeEntry._ID + " INTEGER PRIARY KEY, ";
+        sql = sql + OptotypeDbContract.OptotypeEntry.ID + " TEXT, ";
+        sql = sql + OptotypeDbContract.OptotypeEntry.OPTOTYPECODE + " TEXT, ";
+        sql = sql + OptotypeDbContract.OptotypeEntry.OPTOTYPENAME + " TEXT, ";
+        sql = sql + OptotypeDbContract.OptotypeEntry.IMAGE + " TEXT, ";
+        sql = sql + " UNIQUE ( " + OptotypeDbContract.OptotypeEntry.ID + " ) ";
+        sql = sql +" ) ";
+
+        db.execSQL(sql);
+
+    }
+
+
+}
