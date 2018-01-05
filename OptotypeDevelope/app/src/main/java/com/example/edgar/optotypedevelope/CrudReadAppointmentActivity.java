98028651abd80206450c23c057e000bb2c55dde8
Edgar-PC\Edgar <edglandaeta.15@gmail.com>
2018-01-04 19:36:56 -0430

    se crea una ventana para que el medico cunsulte a los posibles pacientes del día

---
+++ OptotypeDevelope/app/src/main/java/com/example/edgar/optotypedevelope/CrudReadAppointmentActivity.java
@@ -0,0 +1,13 @@
+package com.example.edgar.optotypedevelope;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrudReadAppointmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crud_read_appointment);
+    }
+}
