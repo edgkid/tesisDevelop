6f9f90f56fd4486d6d9ba3573837e94dfb051206
Edgar-PC\Edgar <edglandaeta.15@gmail.com>
2018-03-26 20:51:59 -0430

    Clase dominio de diagnostico

---
+++ OptotypeDevelope/app/src/main/java/com/example/edgar/optotypedevelope/Diagnostic.java
@@ -0,0 +1,144 @@
+package com.example.edgar.optotypedevelope;
+
+/**
+ * Created by Edgar on 26/03/2018.
+ */
+
+public class Diagnostic {
+
+    private String idPatient;
+    private String patient;
+    private String years;
+    private String sex;
+    private String date;
+    private String center;
+    private String sustain;
+    private String maintain;
+    private String avLeft;
+    private String avRigth;
+    private String signalDefect;
+    private String extendsMon;
+    private String extendDad;
+    private String typeTest;
+    private String colaborate;
+
+    public String getIdPatient() {
+        return idPatient;
+    }
+
+    public void setIdPatient(String idPatient) {
+        this.idPatient = idPatient;
+    }
+
+    public String getPatient() {
+        return patient;
+    }
+
+    public void setPatient(String patient) {
+        this.patient = patient;
+    }
+
+    public String getYears() {
+        return years;
+    }
+
+    public void setYears(String years) {
+        this.years = years;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCenter() {
+        return center;
+    }
+
+    public void setCenter(String center) {
+        this.center = center;
+    }
+
+    public String getSustain() {
+        return sustain;
+    }
+
+    public void setSustain(String sustain) {
+        this.sustain = sustain;
+    }
+
+    public String getMaintain() {
+        return maintain;
+    }
+
+    public void setMaintain(String maintain) {
+        this.maintain = maintain;
+    }
+
+    public String getAvLeft() {
+        return avLeft;
+    }
+
+    public void setAvLeft(String avLeft) {
+        this.avLeft = avLeft;
+    }
+
+    public String getAvRigth() {
+        return avRigth;
+    }
+
+    public void setAvRigth(String avRigth) {
+        this.avRigth = avRigth;
+    }
+
+    public String getSignalDefect() {
+        return signalDefect;
+    }
+
+    public void setSignalDefect(String signalDefect) {
+        this.signalDefect = signalDefect;
+    }
+
+    public String getExtendsMon() {
+        return extendsMon;
+    }
+
+    public void setExtendsMon(String extendsMon) {
+        this.extendsMon = extendsMon;
+    }
+
+    public String getExtendDad() {
+        return extendDad;
+    }
+
+    public void setExtendDad(String extendDad) {
+        this.extendDad = extendDad;
+    }
+
+    public String getTypeTest() {
+        return typeTest;
+    }
+
+    public void setTypeTest(String typeTest) {
+        this.typeTest = typeTest;
+    }
+
+    public String getColaborate() {
+        return colaborate;
+    }
+
+    public void setColaborate(String colaborate) {
+        this.colaborate = colaborate;
+    }
+}
